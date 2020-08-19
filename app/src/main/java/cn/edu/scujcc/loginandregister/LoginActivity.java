d8af4c4b13fc18374596cd08736f1e67fa7ecf99
Roger <63828530+haoxiangkaixuea@users.noreply.github.com>
2020-08-19 18:21:32 +0800

    完成验证码的倒计时操作

--- app/src/main/java/cn/edu/scujcc/loginandregister/LoginActivity.java
+++ app/src/main/java/cn/edu/scujcc/loginandregister/LoginActivity.java
@@ -3,11 +3,13 @@ package cn.edu.scujcc.loginandregister;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +23,10 @@ public class LoginActivity extends AppCompatActivity {
     private ImageView imageTellPhone;
     private ImageView imageVerify;
     private ImageView imageBack;
+    private TextView tvGetVerity;
+    private int times = 60;
+    private Handler handler = new Handler();
+    private MyRunnable runnable = new MyRunnable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         editTellPhone = findViewById(R.id.edit_tellPhone);
+        tvGetVerity = findViewById(R.id.get_verify);
         editVerify = findViewById(R.id.edit_verify);
         btnNext = findViewById(R.id.btn_login);
         checkBox = findViewById(R.id.cb_login);
@@ -61,5 +68,28 @@ public class LoginActivity extends AppCompatActivity {
         btnNext.setOnClickListener(v -> {
 
         });
+        tvGetVerity.setOnClickListener(view -> {
+            times = 60;
+            handler.postDelayed(runnable, 1000);
+            tvGetVerity.setTextColor(Color.GRAY);
+        });
+    }
+
+    //倒计时
+    class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            times--;
+            tvGetVerity.setText(times + "s后重新发送");
+            if (times == 0) {
+                tvGetVerity.setText("重新发送");
+                times = 60;
+                tvGetVerity.setClickable(true);
+                tvGetVerity.setTextColor(Color.BLUE);
+            } else {
+                tvGetVerity.setClickable(false);
+                handler.postDelayed(runnable, 1000);
+            }
+        }
     }
 }
