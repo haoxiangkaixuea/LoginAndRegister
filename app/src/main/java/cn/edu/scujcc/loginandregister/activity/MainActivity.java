d44e9b1ee8c1efd8b3d9e5f65aee60749e1ccac6
Roger <63828530+haoxiangkaixuea@users.noreply.github.com>
2020-09-06 14:04:30 +0800

    修改界面上移的问题（未完成)

--- app/src/main/java/cn/edu/scujcc/loginandregister/activity/MainActivity.java
+++ app/src/main/java/cn/edu/scujcc/loginandregister/activity/MainActivity.java
@@ -1,16 +1,65 @@
 package cn.edu.scujcc.loginandregister.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import cn.edu.scujcc.loginandregister.R;
 
 public class MainActivity extends AppCompatActivity {
+    private ScrollView scrollView1;
+
+    /**
+     * 让ScrollView滚动到底部
+     *
+     * @param decorView
+     * @param scroll
+     */
+    public static void scrollToBottom(final View decorView, final View scroll) {
+
+        Handler mHandler = new Handler();
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (scroll == null || decorView == null) {
+                    return;
+                }
+                int offset = decorView.getMeasuredHeight() - scroll.getHeight();
+                if (offset < 0) {
+                    offset = 0;
+                }
+                scroll.scrollTo(0, offset);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        //scrollView1为布局文件中ScrollView的id
+        // 监听键盘弹起或关闭(ScrollView)
+        scrollView1 = findViewById(R.id.scrollView2);
+        scrollView1.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(final View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                if (oldBottom != 0 && bottom != 0 && (oldBottom - bottom > 100)) {
+//                    Toast.makeText(getApplicationContext(), "监听到软键盘弹起...", Toast.LENGTH_SHORT).show();
+//                    scrollView.fullScroll(ScrollView.FOCUS_DOWN);(会导致输入框失去焦点)
+                    // 获得屏幕
+                    View decorView = getWindow().getDecorView();
+                    scrollToBottom(decorView, v);
+                } else if (oldBottom != 0 && bottom != 0 && (bottom - oldBottom > 100)) {
+//                    Toast.makeText(getApplicationContext(), "监听到软件盘关闭...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/cn/edu/scujcc/loginandregister/util/KeyboardUtils.java
@@ -0,0 +1,36 @@
+package cn.edu.scujcc.loginandregister.util;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by matth on 2018/6/13.
+ */
+
+public class KeyboardUtils {
+    public static void showKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            view.requestFocus();
+            imm.showSoftInput(view, 0);
+        }
+    }
+
+    public static void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public static void toggleSoftInput(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.toggleSoftInput(0, 0);
+        }
+    }
+}
